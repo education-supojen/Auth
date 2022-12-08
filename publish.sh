50595f57a6aae4b5098212a41816b5c0085676ad
Po <brian71742@gmail.com>
2022-12-08 15:54:41 +0800

    GG75

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ directory="./compile"
 if [ -d $directory ]; then
   echo "Directory exsits"
 else
-  dotnet restore
+  dotnet restore -NoCache -NonInteractive
   dotnet publish src/Auth.Presentation/Auth.Presentation.csproj -r linux-x64 --self-contained false --configuration Release -o compile
 fi 
